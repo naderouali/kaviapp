a6e9155f15cd3d588a74299fdac947d4090c1a02
nader <oualinader@gmail.com>
2020-11-25 16:18:03 +0100

    working add buttons

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { Stage, Layer, Rect, Text, Circle, Line, Image, Shape } from 'react-konva';
+import { Stage, Layer, Rect, Text, Circle, Line, Image, Shape, Group } from 'react-konva';
 import ReactDOM from 'react-dom';
 import Portal from './Portal.js';
 import useImage from 'use-image';
@@ -27,34 +27,14 @@ export default function Tools() {
     const dragUrl = useRef();
     const stageRef = useRef();
     const [images, setImages] = useState([]);
-    const [shapes, setShapes] = useState([{
-        id: 0,
-        type: "rect",
-        fill: "#d62828",
-        x: 800,
-        y: 50,
-        width: 200,
-        height: 50
-    }, {
-        id: 1,
-        type: "rect",
-        fill: "purple",
-        fill: "#fca311",
-        x: 800,
-        y: 130,
-        width: 200,
-        height: 50
-    }, {
-        id: 2,
-        type: "rect",
-        fill: "blue",
-        fill: "#48cae4",
-        x: 800,
-        y: 250,
-        width: 200,
-        height: 50
-    }
-    ]);
+    const [shapes, setShapes] = useState([]);
+    const [input, setInput] = useState([]);
+
+    const onChangeInput = (e) => {
+        setInput(e.target.value);
+        //console.log(e.target.value);
+    };
+
 
 
 
@@ -86,19 +66,19 @@ export default function Tools() {
 
     return (
         <>
-            <div style={{ backgroundImage: `url(${Mockup})`, backgroundSize: 270, position: "absolute", top: 100, left: 50, width: 270, height: 550, backgroundRepeat: "no-repeat" }} />
+
+            <div style={{ backgroundImage: `url(${Mockup})`, backgroundSize: 270, position: "absolute", top: 114, left: 50, width: 270, height: 550, backgroundRepeat: "no-repeat" }} />
 
             <div style={{ backgroundColor: "#344073", marginTop: 70, display: "flex", flexDirection: "row" }}>
-                <div
-                    style={{ backgroundImage: `url(${fakeButton})`, position: "fixed", top: 50, zIndex: 30000, height: "100%", width: "100%", backgroundRepeat: "no-repeat" }}
+                {/* <div
+                    style={{ backgroundImage: `url(${fakeButton})`, position: "fixed", top: 50, zIndex: 30000, height: "100px", width: "100px", backgroundRepeat: "repeat" }}
 
                     draggable
                     onDragOver={e => e.preventDefault()}
                     onDragStart={e => {
                         dragUrl.current = e.target.src;
                     }}
-                ></div>
-                <button style={{ backgroundImage: `url(${fakeButton})` }} onClick={console.log("okayyyyyyyyyyyy")}>test</button>
+                ></div> */}
 
                 <div
                     onDrop={e => {
@@ -122,152 +102,119 @@ export default function Tools() {
                             zIndex: 3000,
                             border: "2px solid red",
                             marginLeft: 67,
-                            marginTop: -78,
+                            marginTop: 78,
                             width: 232,
                             height: 485
                         }}>
 
-                        <Layer style={{ marginLeft: 490 }}>
-                            {images.map(image => {
-                                return <URLImage image={image} />;
-                            })}
-                        </Layer>
-                        <Layer>
-                            <Rect
-                                x={200}
-                                y={80}
-                                width={40}
-                                height={60}
-                                fill={"red"}
-                                draggable
-                                onDragStart={e => {
-                                    dragUrl.current = e.target.src;
-                                }}
-                            />
-                        </Layer>
-                    </Stage>
 
-                    <button onClick={() => {
-                        let temp = shapes;
+                        <Layer >
+                            {/* in this layer we spawn the buttons and texts */}
 
-                        temp.push({
-                            id: shapes.length + 1,
-                            type: "rect",
-                            fill: "#48cae4",
-                            x: 300,
-                            y: 80,
-                            width: 200,
-                            height: 50
-                        })
-
-                        setShapes([...temp])
-
-                    }} >Add button</button>
-                </div>
-            </div>
-
-
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <div>
-                <div style={{ backgroundColor: "#344073", display: "flex", flexDirection: "row", marginTop: 70 }}>
-                    <button onClick={() => {
-                        let temp = shapes;
-
-                        temp.push({
-                            id: shapes.length + 1,
-                            type: "rect",
-                            fill: "blue",
-                            fill: "#48cae4",
-                            x: 80,
-                            y: 80,
-                            width: 200,
-                            height: 50
-                        })
-
-                        setShapes([...temp])
-
-                    }} >tttt</button>
-
-
-                    <Stage width={window.innerWidth} height={window.innerHeight} style={{ zIndex: 200, border: "16px solid green" }}>
-
-                        <Layer>
-                            <Image
-                                x={60}
-                                y={15}
-                                width={window.innerHeight / 1.7}
-                                height={window.innerHeight / 1.16}
-                                image={image}
-                            />
-                        </Layer>
-                        <Layer>
-                            <Rect
-                                x={80}
-                                y={80}
-                                width={40}
-                                height={60}
-                                fill={"green"}
-                                draggable
-                                onDragStart={e => {
-                                    dragUrl.current = e.target.src;
-                                }}
-                            />
-                        </Layer>
-                        <Layer>
                             {shapes.map(shape => (
                                 <>
                                     {
-                                        (shape.type === "rect") && <Rect
-                                            key={shape.id}
-                                            id={shape.id}
-                                            x={shape.x}
-                                            y={shape.y}
-                                            width={shape.width}
-                                            height={shape.height}
-                                            fill={shape.fill}
-                                            shadowBlur={10}
-                                            draggable
-                                            scaleX={shape.isDragging ? 1.2 : 1}
-                                            scaleY={shape.isDragging ? 1.2 : 1}
-                                            onDragStart={handleDragStart}
-                                            onDragEnd={handleDragEnd}
+                                        (shape.type === "rect") &&
+                                        <Group draggable>
+                                            <Rect
+                                                style={{ position: "absolute" }}
+                                                key={shape.id}
+                                                id={shape.id}
+                                                x={shape.x}
+                                                y={shape.y}
+                                                width={shape.width}
+                                                height={shape.height}
+                                                fill={shape.fill}
+                                                shadowBlur={2}
+                                                scaleX={shape.isDragging ? 1.2 : 1}
+                                                scaleY={shape.isDragging ? 1.2 : 1}
+                                                onDragStart={handleDragStart}
+                                                onDragEnd={handleDragEnd}
+                                            />
+                                            <Text text={shape.content} fontSize={18} fill={'black'} x={shape.x + 6} y={shape.y + 6} width={shape.width} height={shape.height}></Text>
 
-                                        />
+                                        </Group>
+
                                     }
 
-                                    {
-                                        (shape.type === "circle") && <Circle
-                                            key={shape.id}
-                                            id={shape.id}
-                                            x={100}
-                                            y={100}
-                                            radius={50}
-                                            fill={shape.fill}
-                                            stroke="yellow"
-                                            strokeWidth={4}
-                                            shadowOffset="20"
-                                            shadowBlur="10"
-                                            draggable
-                                            scaleX={shape.isDragging ? 1.2 : 1}
-                                            scaleY={shape.isDragging ? 1.2 : 1}
-                                            onDragStart={handleDragStart}
-                                            onDragEnd={handleDragEnd}
-                                        />
-                                    }
                                 </>
+
                             ))}
+
+                        </Layer>
+
+                        <Layer >
+
+                            <Group draggable style={{ position: "absolute" }} zIndex={50000} >
+
+                                <Rect style={{ position: "absolute" }} fill={"yellow"} width={50} height={50} zIndex={50000} />
+                                <Text text={'slm'} fontSize={20} />
+
+
+
+                            </Group>
+                        </Layer>
+
+
+                        {/* layer of the input text of the button */}
+                        <Layer >
+                            <Portal>
+                                <input
+                                    onChange={onChangeInput}
+                                    style={{
+                                        position: 'absolute',
+                                        top: 180,
+                                        right: 450,
+                                        width: '160px',
+                                        height: 30,
+                                        backgroundColor: 'white',
+                                        textDecoration: 'none',
+                                        border: 'none'
+                                    }}
+                                    placeholder="your button text here.."
+                                />
+                            </Portal>
                         </Layer>
                     </Stage>
 
+                    <button style={{ position: "absolute", top: 180, right: 300, backgroundColor: "#48cae4", color: "white", paddingRight: 30, paddingLeft: 30, paddingTop: 10, paddingBottom: 10, border: "none", fontSize: 14 }}
+                        onClick={() => {
+                            let temp = shapes;
 
+                            temp.push({
+                                id: shapes.length + 1,
+                                type: "rect",
+                                fill: "#48cae4",
+                                x: 100,
+                                y: 40,
+                                width: 100,
+                                height: 30,
+                                content: input
+                            })
+
+                            setShapes([...temp])
+                        }} >Add button</button>
+
+                    <button style={{ position: "absolute", top: 180, right: 150, backgroundColor: "red", color: "white", paddingRight: 30, paddingLeft: 30, paddingTop: 10, paddingBottom: 10, border: "none", fontSize: 14 }}
+                        onClick={() => {
+                            let temp = shapes;
+
+                            temp.push({
+                                id: shapes.length + 1,
+                                type: "rect",
+                                fill: "red",
+                                x: 100,
+                                y: 40,
+                                width: 100,
+                                height: 30,
+                                content: input
+                            })
+
+                            setShapes([...temp])
+                        }} >Add button</button>
                 </div>
             </div>
+
         </>
     )
 }
